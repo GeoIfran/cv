8bd345a2c434aa7c41723fe908c095b3f280fe56
Georgina Ifran <geovifran@gmail.com>
2023-01-24 02:02:48 -0300

    trabajo fallido con datos de randomuser

---
+++ js/persona2.js
@@ -0,0 +1,23 @@
+const url = 'https://randomuser.me/api/?nat=es';
+let persona = "x";
+function profesionSexista (sexo){
+    if(sexo == "male"){
+        return 'Experto en refrigeración de materiales de la construcción';
+    }else{
+        return 'Experta en refrigeración de materiales de la construcción';
+    }
+};
+/**dejo de andar y no entiendo porque */
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+    let persona = data.results
+    console.log(persona)
+    persona.map(function(persona){
+        document.getElementById("nombreCompleto-titulo").innerHTML = `${persona.name.last}, ${persona.name.first}`
+
+    })
+    
+}); 
+
+ 
